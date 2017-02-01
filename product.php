2533a24d8b8de1cfc4e76f1808f9ef166f537c1c
CoderDam <garnes.damien@gmail.com>
2017-02-01 10:18:49 +0100

    ajout vérification GET pour consulter page produit

--- product.php
+++ product.php
@@ -1,6 +1,22 @@
 <?php
 include("inc/data.php");
-$id = (isset($_GET["id"])) ? $_GET["id"] : 0;
+
+// tester si $_GET n'est pas numérique,
+// OU si rien n'est saisi = "id"
+// on renvoie vers l'accueil
+if (!is_numeric($_GET["id"]) OR !isset($_GET["id"])) {
+	header("Location: index.php");
+}
+
+// sinon, on récupère le $_GET
+$id = $_GET["id"];
+
+// MAIS, s'il n'y a pas de produit à l'index demandé,
+// on renvoie aussi vers l'accueil
+if (!isset($products[$id])) {
+	header("Location: index.php");
+}
+
 include("templates/header.php");
 include("templates/product-desc.php");
 include("templates/footer.php");
